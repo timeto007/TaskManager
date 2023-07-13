66867e7914cc159abc1e8ae66056055fa8582f6b
Nesrudheen V P <48428588+timeto007@users.noreply.github.com>
2023-07-13 17:41:03 +0530

    Update 3
    we can upload image of Task

--- src/main/java/com/testCRUD/NewCrud/NewCrud.java
+++ src/main/java/com/testCRUD/NewCrud/NewCrud.java
@@ -2,8 +2,10 @@ package com.testCRUD.NewCrud;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
+@ComponentScan(basePackages = {"com.testCRUD.NewCrud.imageRepo"})
 public class NewCrud {
 
 	public static void main(String[] args) {
